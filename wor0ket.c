a98dc21900c2ce12497a06fd35a859c9b96d061e
Wilston Oreo <mirrorpoolmusic@gmx.net>
2011-12-14 00:57:20 +0100

    Source code improvements

--- wor0ket.c
+++ wor0ket.c
@@ -14,6 +14,8 @@ static u8* pDisplayBuf;
 float posX = 65536.0;
 float posY = 65536.0;
 
+typedef float Vector[3];
+
 float time = 0;
 
 void setDispPixel(int _x, int _y, u8 value)
@@ -39,133 +41,161 @@ void setPixel(int x, int y, u8 value)
 	pPixelBuf[y*XSIZE+x] = value;
 }
 
+void addPixel(int x, int y, u8 value)
+{
+	pPixelBuf[y*XSIZE+x] = value;
+}
+
+float sphereIntersection(Vector spherePos, Vector rayOrg, Vector rayDir)
+{
+	#define RADIUS 1.6
+
+	float a,b,c,t = -100.0;
+
+	float ocVec[3];
+	ocVec[0] = rayOrg[0] - spherePos[0];
+	ocVec[1] = rayOrg[1] - spherePos[1];
+	ocVec[2] = rayOrg[2] - spherePos[2];
+
+	a = rayDir[0]*rayDir[0] + rayDir[1]*rayDir[1] + rayDir[2]*rayDir[2];
+	b = 2*(ocVec[0]*rayDir[0]+ocVec[1]*rayDir[1]+ocVec[2]*rayDir[2]);
+	c = ocVec[0]*ocVec[0]+ocVec[1]*ocVec[1]+ocVec[2]*ocVec[2]  - (RADIUS*RADIUS);
+
+	float disc = b * b - 4 * a * c;
+
+#define BORDER 0.25f
+	if (disc >= -BORDER && disc <= BORDER)
+	{
+		return 0;
+	}
+
+	if (disc < -BORDER) { //setPixel(x,y,0); 
+		return t;
+
+	}
+
+	float distSqrt = sqrtf(disc);
+	float q;
+	if (b < 0)
+		q = (-b - distSqrt)/2.0;
+	else
+		q = (-b + distSqrt)/2.0;
+
+    // compute t0 and t1
+    float t0 = q / a;
+    float t1 = c / q;
+
+    // make sure t0 is smaller than t1
+    if (t0 > t1)
+    {
+        // if t0 is bigger than t1 swap them around
+        float temp = t0;
+        t0 = t1;
+        t1 = temp;
+    }
+
+    // if t1 is less than zero, the object is in the ray's negative direction
+    // and consequently the ray misses the sphere
+   if (t1 < 0) return t1;
+
+
+    // if t0 is less than zero, the intersection point is at t1
+    if (t0 < 0)
+    {
+        t = t1;
+    }
+    // else the intersection point is at t0
+    else
+    {
+        t = t0;
+    }
+
+
+//printf("%f \t%f \t%f \t --- %f \t%f \t%f \t --- %f\n",rayOrg[0],rayOrg[1],rayOrg[2],rayDir[0],rayDir[1],rayDir[2],t);
+	return t;
+}
+
+float planeIntersection(Vector rayOrg, Vector rayDir)
+{
+	 return -(4.0-rayOrg[1])/(rayDir[1]);
+}
+
+u8 checkerBoard(float t, float x, float z)
+{
+	float dir = (t > 0) ? 1 : -1;
+	float xC = (x*0.15) + dir*posX;
+	float zC = (z*0.15) - dir*posY;
+
+	int checker = ((int)xC % 2 + (int)zC%2) % 2;
+
+	int dist = 128 - (int)sqrt(x*x + z*z)*2;
+	if (dist < 0) dist = 0; 
+
+	return dist*(int)checker;
+}
+
+
 void draw()
 {
-	#define RADIUS 1.5
 	int x,y;
-	float ray_org[3],a,b,c,t;
+	float rayOrg[3],rayDir[3];
 
-	float spherePos[3];
-	spherePos[0] = 1.2*sin(time/400);
+	Vector spherePos;
 	spherePos[1] = 0.0f;
-	spherePos[2] = 2*cos(time/1200);
+	spherePos[2] = 2*cos(time/1600)+1;
+	spherePos[0] = 1.2*sin(time/400);
+
+
+	for (y = 0; y < YSIZE; y++)
+		for (x = 0; x < XSIZE; x++)
+			setPixel(x,y,0);
 
 	for (y = 0; y < YSIZE; y++)
 		for (x = 0; x < XSIZE; x++)
 		{
-			ray_org[0] = 0;
-			ray_org[1] = 0;
-			ray_org[2] = -3.5;
+			float t = 0;
+			rayOrg[0] = 0;
+			rayOrg[1] = 0;
+			rayOrg[2] = -3.5;
 
-			float ray_dir[3];
-			ray_dir[0] = 1.5*((float)x/(float)(XSIZE)-0.5f);
-			ray_dir[1] = 1.5*((float)y/(float)(YSIZE)*0.5f*(float)XSIZE/(float)YSIZE-0.33f);
-			ray_dir[2] = -1;
+			float rayDir[3];
+			rayDir[0] = 1.5*((float)x/(float)(XSIZE)-0.5f);
+			rayDir[1] = 1.5*((float)y/(float)(YSIZE)*0.5f*(float)XSIZE/(float)YSIZE-0.33f);
+			rayDir[2] = 1;
 
-			float ocVec[3];
-			ocVec[0] = ray_org[0] - spherePos[0];
-			ocVec[1] = ray_org[1] - spherePos[1];
-			ocVec[2] = ray_org[2] - spherePos[2];
-
-
-			a = ray_dir[0]*ray_dir[0] + ray_dir[1]*ray_dir[1] + 1;
-			b = 2*(ocVec[0]*ray_dir[0]+ocVec[1]*ray_dir[1]+ocVec[2]*ray_dir[2]);
-
-			c = ocVec[0]*ocVec[0]+ocVec[1]*ocVec[1]+ocVec[2]*ocVec[2]  - (RADIUS*RADIUS);
-
-			t = 0;
-    		float disc = b * b - 4 * a * c;
-    
-#define BORDER 0.25f
-			if (disc >= -BORDER && disc <= BORDER)
+			t = sphereIntersection(spherePos,rayOrg,rayDir);
+			if (t < 0) 
+			{
+				t = planeIntersection(rayOrg,rayDir);
+				rayOrg[0] += t*rayDir[0];
+				rayOrg[2] += t*rayDir[2];
+				setPixel(x,y,checkerBoard(t,rayOrg[0],rayOrg[2])); 
+			} else/*
+			if (t == 0)
 			{
 				setPixel(x,y,128);
-				continue;
 			}
-
-		    if (disc < BORDER) { //setPixel(x,y,0); 
-
-				t = -(4.0-ray_org[1])/(ray_dir[1]);
-
-				ray_org[0] = ray_org[0] + t*ray_dir[0];
-				ray_org[1] = ray_org[1] + t*ray_dir[1];
-				ray_org[2] = ray_org[2] + t*ray_dir[2];
-
-				float dir = (t > 0) ? 1 : -1;
-
-				float xC = (ray_org[0]*0.15) + dir*posX;
-				float zC = (ray_org[2]*0.15) - dir*posY;
-				if (ray_org[0] < 0) ray_org[0] -= 1;
-
-				int   checker = ((int)xC % 2 + (int)zC%2) % 2;
-
-				int dist = 128 - (int)sqrt(ray_org[0]*ray_org[0]/6 + ray_org[2]*ray_org[2]/6)*12;
-				if (dist < 0) dist = 0; 
-
-				setPixel(x,y,dist*(int)checker);
-				continue; 
-			}
-
-			float distSqrt = sqrtf(disc);
-    		float q;
-    		if (b < 0)
-        		q = (-b - distSqrt)/2.0;
-    		else
-        		q = (-b + distSqrt)/2.0;
-
-    		float t0 = q / a;
-   			//float t1 = c / q;
-
-
-    		//if (t1 < 0) continue;
-				
-			if (t0 < 0)
-    		{
-        		t = t0;
-				setPixel(x,y,128); 
-    		}
-    		else
-    		{
-        		t = t0;
-				setPixel(x,y,64); 
-    		}
-
-			ray_org[0] = ray_org[0] + t*ray_dir[0];
-			ray_org[1] = ray_org[1] + t*ray_dir[1];
-			ray_org[2] = ray_org[2]  + t*ray_dir[2];
-			ocVec[0] = ray_org[0] - spherePos[0];
-			ocVec[1] = ray_org[1] - spherePos[1];
-			ocVec[2] = ray_org[2] - spherePos[2];
-
-
-			float reflProd = 2*(ocVec[0]*ray_dir[0] + ocVec[1]*ray_dir[1] + ocVec[2]*ray_dir[2]);
-
-			ray_dir[0] = ray_dir[0] - reflProd * ocVec[0] ;
-			ray_dir[1] = ray_dir[1] - reflProd * ocVec[1] ;
-			ray_dir[2] = ray_dir[2] - reflProd * ocVec[2] ;
-
-			t = -(4.0-ray_org[1])/(ray_dir[1]);
-	//		printf("%f \t%f \t%f \t --- %f \t%f \t%f \t --- %f\n",ray_org[0],ray_org[1],ray_org[2],ray_dir[0],ray_dir[1],ray_dir[2],t);
-
-			//if (t > 0)
+			else*/
 			{
-				ray_org[0] = ray_org[0] + t*ray_dir[0];
-				ray_org[1] = ray_org[1] + t*ray_dir[1];
-				ray_org[2] = ray_org[2] + t*ray_dir[2];
+				rayOrg[0] = rayOrg[0] + t*rayDir[0];
+				rayOrg[1] = rayOrg[1] + t*rayDir[1];
+				rayOrg[2] = rayOrg[2] + t*rayDir[2];
 
-				float dir = (t > 0) ? 1 : -1;
+				Vector ocVec;
+				ocVec[0] = rayOrg[0] - spherePos[0];
+				ocVec[1] = rayOrg[1] - spherePos[1];
+				ocVec[2] = rayOrg[2] - spherePos[2];
+		//		printf("%f \t%f \t%f \t --- %f \t%f \t%f \t --- %f\n",rayOrg[0],rayOrg[1],rayOrg[2],rayDir[0],rayDir[1],rayDir[2],t);
 
-				float xC = (ray_org[0]*0.15) + dir*posX;
-				float zC = (ray_org[2]*0.15) - dir*posY;
-				if (ray_org[0] < 0) ray_org[0] -= 1;
+				float reflProd = 2*(ocVec[0]*rayDir[0] + ocVec[1]*rayDir[1] + ocVec[2]*rayDir[2]);
+				rayDir[0] = rayDir[0] - reflProd * ocVec[0] ;
+				rayDir[1] = rayDir[1] - reflProd * ocVec[1] ;
+				rayDir[2] = rayDir[2] - reflProd * ocVec[2] ;
 
-				int   checker = ((unsigned int)xC % 2 + (unsigned int)zC % 2) % 2;
-				
-
-				int dist = 128 - (int)(ray_org[0]*ray_org[0] + ray_org[2]*ray_org[2])/3;
-				if (dist < 0) dist = 0; 
-				
-				setPixel(x,y,(128*checker)*dist/128);
+				t = planeIntersection(rayOrg,rayDir);
+				rayOrg[0] += t*rayDir[0];
+				rayOrg[2] += t*rayDir[2];
+				setPixel(x,y,checkerBoard(t,rayOrg[0],rayOrg[2])); 
 			}
 		}
 }
@@ -179,25 +209,13 @@ void dither()
 		{
 			int oldPixel = pPixelBuf[y*XSIZE+x];
 			int newPixel = oldPixel/128*128;
-			//printf("%d ",newPixel/64);
 
 			int quantError = oldPixel - newPixel;
-			
-			/* // Floyd-Steinberg
-		    pPixelBuf[y*XSIZE+x+1    ] += 7 * quantError / 16;
-     		pPixelBuf[(y+1)*XSIZE+x-1] += 3 * quantError / 16;
-      	    pPixelBuf[(y+1)*XSIZE+x  ] += 5 * quantError / 16;
-      		pPixelBuf[(y+1)*XSIZE+x+1] += 1 * quantError / 16;*/
 		
-			pPixelBuf[y*XSIZE+x+1    ] += quantError / 4;
-			pPixelBuf[y*XSIZE+x+2    ] += quantError / 8;
-			pPixelBuf[(y+1)*XSIZE+x-1] += quantError / 8;
+			pPixelBuf[y*XSIZE+x+1    ] += quantError / 2;
+			pPixelBuf[(y+1)*XSIZE+x-1] += quantError / 4;
 			pPixelBuf[(y+1)*XSIZE+x  ] += quantError / 4;
-			pPixelBuf[(y+1)*XSIZE+x+1] += quantError / 8;
-			pPixelBuf[(y+2)*XSIZE+x  ] += quantError / 8;
-
 		
-
 			pPixelBuf[y*XSIZE+x] = newPixel/128*255;
 		}
 	}
@@ -228,8 +246,14 @@ void update(int value)
 {
 	glutPostRedisplay();
 
-	posX += 0.07*sin(time/2000);
-	posY += 0.05;
+	float dX = 0.07*sin(time/2000), dY = 0.05;
+	float l = sqrt(dX*dX + dY*dY); 
+	dX /= l*12; dY /= l*12;
+
+	posX += dX;
+	posY += dY;
+
+
 	time += 1000/24;
 
     glutTimerFunc(1000/24, update, 0);
